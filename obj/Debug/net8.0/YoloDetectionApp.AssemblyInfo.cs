6808b629ae4065a0a5f36b16a54f3466ec155c81
Fthgzlkr <f211220034@ktun.edu.tr>
2025-05-17 20:33:57 +0300

    Add viewmodel

--- obj/Debug/net8.0/YoloDetectionApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/YoloDetectionApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YoloDetectionApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e045cbba9189b712b55181d1c8084c9dbc08f6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b903c1e7c4d8762d1eb469e6416d33b5e0e519")]
 [assembly: System.Reflection.AssemblyProductAttribute("YoloDetectionApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YoloDetectionApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
